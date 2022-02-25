be73c34f574166df35f92c7402fddd234bb0ef9e
Claudeilton Dantas <fcd007@hotmail.com>
2022-02-24 23:07:44 -0300

    feat: estrutura base do projeto gestao de vendas

---
+++ src/test/java/com/javacode/javacode/JavacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacode.javacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
